656985cbdea1bb232a7c9a627e705fa2dd3663a0
Aitor Pagan <aitor.pagan@apiumhub.com>
2023-12-04 16:36:42 +0100

    Day four challenge part 2

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,36 +1,64 @@
 import kotlin.math.pow
-data class CardGame(val id: String, val winners: List<Int>, val played: List<Int>) {
+data class CardGame(val id: Int, val winners: List<Int>, val played: List<Int>) {
     fun prize(): Int {
         val result = winners.filter { played.contains(it) }.fold(0) { acc, _ -> acc + 1 }
         return 2.0.pow(result - 1).toInt()
     }
+
+    fun matches(): Int {
+        return winners.count { played.contains(it) }
+    }
+
+    var copies: Int = 1
 }
 fun main() {
     fun part1(input: List<String>): Int {
         return input.map { inp ->
             val splitString = inp.split(":")
-            val gameId = splitString.first().removePrefix("Card ")
+            val gameId = splitString.first().removePrefix("Card ").trim().toInt()
             val games = splitString.last().split("|")
-            val winners = games.first().split(" ").map { it.toIntOrNull() }.filterNotNull()
-            val played = games.last().split(" ").map { it.toIntOrNull() }.filterNotNull()
+            val winners = games.first().split(" ").mapNotNull { it.toIntOrNull() }
+            val played = games.last().split(" ").mapNotNull { it.toIntOrNull() }
             CardGame(gameId, winners, played)
         }
             .fold(0) { acc, i -> acc + i.prize()}
     }
 
+    fun calculateTotalMatches(input: List<CardGame>): Int {
+        val mutableList = input.toMutableList()
+        for (index in mutableList.indices) {
+            for (match in (index + 1)..index + mutableList[index].matches()) {
+                if (match < mutableList.size) {
+                    mutableList[match].copies += mutableList[index].copies
+                }
+            }
+        }
+        return mutableList.sumOf { it.copies }
+    }
+
     fun part2(input: List<String>): Int {
-        return input.size
+        val cards = input.map { inp ->
+            val splitString = inp.split(":")
+            val gameId = splitString.first().removePrefix("Card ").trim().toInt()
+            val games = splitString.last().split("|")
+            val winners = games.first().split(" ").mapNotNull { it.toIntOrNull() }
+            val played = games.last().split(" ").mapNotNull { it.toIntOrNull() }
+            CardGame(gameId, winners, played)
+        }
+
+        return calculateTotalMatches(cards)
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day04_tests")
-//    part1(testInput).println()
+    part1(testInput).println()
     check(part1(testInput) == 13)
-//    check(part2(testInput) == 2286)
+    check(part2(testInput) == 30)
+    part2(testInput).println()
 
     val input = readInput("Day04")
     part1(input).println()
-//    part2(input).println()
+    part2(input).println()
 }
 
 /*
@@ -67,5 +95,35 @@ Card 6 has no winning numbers, so it is worth no points.
 So, in this example, the Elf's pile of scratchcards is worth 13 points.
 
 Take a seat in the large pile of colorful cards. How many points are they worth in total?
+
+--- Part Two ---
+Just as you're about to report your findings to the Elf, one of you realizes that the rules have actually been printed on the back of every card this whole time.
+
+There's no such thing as "points". Instead, scratchcards only cause you to win more scratchcards equal to the number of winning numbers you have.
+
+Specifically, you win copies of the scratchcards below the winning card equal to the number of matches. So, if card 10 were to have 5 matching numbers, you would win one copy each of cards 11, 12, 13, 14, and 15.
+
+Copies of scratchcards are scored like normal scratchcards and have the same card number as the card they copied. So, if you win a copy of card 10 and it has 5 matching numbers, it would then win a copy of the same cards that the original card 10 won: cards 11, 12, 13, 14, and 15. This process repeats until none of the copies cause you to win any more cards. (Cards will never make you copy a card past the end of the table.)
+
+This time, the above example goes differently:
+
+Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+Card 1 has four matching numbers, so you win one copy each of the next four cards: cards 2, 3, 4, and 5.
+Your original card 2 has two matching numbers, so you win one copy each of cards 3 and 4.
+Your copy of card 2 also wins one copy each of cards 3 and 4.
+Your four instances of card 3 (one original and three copies) have two matching numbers, so you win four copies each of cards 4 and 5.
+Your eight instances of card 4 (one original and seven copies) have one matching number, so you win eight copies of card 5.
+Your fourteen instances of card 5 (one original and thirteen copies) have no matching numbers and win no more cards.
+Your one instance of card 6 (one original) has no matching numbers and wins no more cards.
+Once all of the originals and copies have been processed, you end up with 1 instance of card 1, 2 instances of card 2, 4 instances of card 3, 8 instances of card 4, 14 instances of card 5, and 1 instance of card 6. In total, this example pile of scratchcards causes you to ultimately have 30 scratchcards!
+
+Process all of the original and copied scratchcards until no more scratchcards are won. Including the original set of scratchcards, how many total scratchcards do you end up with?
+
+
 
 * */
