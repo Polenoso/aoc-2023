567d218ca9f28625e6bda040a641df3124d208fe
Aitor Pagan <aitor.pagan@apiumhub.com>
2023-12-11 12:48:31 +0100

    Day ten challenge part 2 (uncomplete)

--- src/Day10.kt
+++ src/Day10.kt
@@ -28,6 +28,14 @@ fun parseInput(input: List<String>): List<MetalPipe> {
         }
     }.flatten()
 }
+
+fun parseInput2(input: List<String>): List<List<MetalPipe>> {
+    return input.mapIndexed { rowIndex, row ->
+        row.mapIndexed { columnIndex, pipeChar ->
+            MetalPipe(pipeChar, rowIndex, columnIndex)
+        }
+    }
+}
 fun main() {
     fun part1(input: List<String>): Int {
         val pipeMap = parseInput(input)
@@ -78,6 +86,69 @@ fun main() {
         return visitedNodes.filter { it.distance != Int.MAX_VALUE }.maxOf { it.distance }
     }
 
+    fun part2(input: List<String>): Int {
+        val pipeMap = parseInput(input)
+        val start = pipeMap.first { it.char == 'S' }
+        val startNeighbors = listOf<Pair<Int, Int>>(Pair(start.x - 1, start.y), Pair(start.x, start.y - 1), Pair(start.x + 1, start.y), Pair(start.x, start.y + 1))
+        val validNeighbors = emptyList<MetalPipe>().toMutableList()
+        for (neigh in startNeighbors) {
+            if (neigh.first < 0 || neigh.second < 0) { continue }
+            val pipe = pipeMap.first { it.x == neigh.first && it.y == neigh.second }
+            if (pipe.neighbors()?.first == Pair(start.x, start.y) || pipe.neighbors()?.second == Pair(start.x, start.y)) {
+                validNeighbors.add(pipe)
+            }
+        }
+        val validCoordinates = validNeighbors.map { Pair(it.x, it.y) }
+        val startPipe = listOf('-', '|', '7', 'F', 'J', 'L').firstNotNullOf {
+            val pipe = MetalPipe(it, start.x, start.y)
+            if (listOf(pipe.neighbors()!!.first, pipe.neighbors()!!.second).containsAll(validCoordinates)) {
+                pipe
+            } else {
+                null
+            }
+        }
+        val visitedNodes = emptyList<MetalPipe>().toMutableList()
+        var nodes = emptyList<MetalPipe>().toMutableList()
+        nodes.add(startPipe)
+        var step = 0
+        while (nodes.isNotEmpty()) {
+            val node = nodes.first()
+            nodes = nodes.drop(1).toMutableList()
+            val neighbors = pipeMap.filter {
+                val adj = node.neighbors() ?: return@filter false
+                val pair = Pair(it.x, it.y)
+                adj.first == pair || adj.second == pair
+            }
+            visitedNodes.add(node)
+            for (neighbor in neighbors) {
+                if (!visitedNodes.contains(neighbor)) {
+                    if (node.distance == Int.MAX_VALUE) {
+                        neighbor.distance = 1
+                    } else {
+                        neighbor.distance = node.distance + 1
+                    }
+                    nodes.add(neighbor)
+                }
+            }
+            step++
+        }
+        val possiblePipes = pipeMap.filter { !visitedNodes.contains(it) }
+
+        val map2 = parseInput2(input)
+        map2.forEach { pipeRow ->
+            pipeRow.forEach { pipe ->
+                if (visitedNodes.contains(pipe)) {
+                    print(' ')
+                } else {
+                    print(pipe.char)
+                }
+            }
+            "".println()
+        }
+
+        return input.size
+    }
+
 
     // Tests
     val testInput = readInput("Day10_tests")
@@ -88,7 +159,10 @@ fun main() {
     check(part1(testInput2) == 8)
 
     val input = readInput("Day10")
-    part1(input).println()
+    //part1(input).println()
+
+    val testInput3 = readInput("Day10_tests3")
+    part2(testInput3).println()
 }
 
 /*
@@ -186,4 +260,95 @@ Here are the distances for each tile on that loop:
 23...
 Find the single giant loop starting at S. How many steps along the loop does it take to get from the starting position to the point farthest from the starting position?
 
+--- Part Two ---
+You quickly reach the farthest point of the loop, but the animal never emerges. Maybe its nest is within the area enclosed by the loop?
+
+To determine whether it's even worth taking the time to search for such a nest, you should calculate how many tiles are contained within the loop. For example:
+
+...........
+.S-------7.
+.|F-----7|.
+.||.....||.
+.||.....||.
+.|L-7.F-J|.
+.|..|.|..|.
+.L--J.L--J.
+...........
+The above loop encloses merely four tiles - the two pairs of . in the southwest and southeast (marked I below). The middle . tiles (marked O below) are not in the loop. Here is the same loop again with those regions marked:
+
+...........
+.S-------7.
+.|F-----7|.
+.||OOOOO||.
+.||OOOOO||.
+.|L-7OF-J|.
+.|II|O|II|.
+.L--JOL--J.
+.....O.....
+In fact, there doesn't even need to be a full tile path to the outside for tiles to count as outside the loop - squeezing between pipes is also allowed! Here, I is still within the loop and O is still outside the loop:
+
+..........
+.S------7.
+.|F----7|.
+.||OOOO||.
+.||OOOO||.
+.|L-7F-J|.
+.|II||II|.
+.L--JL--J.
+..........
+In both of the above examples, 4 tiles are enclosed by the loop.
+
+Here's a larger example:
+
+.F----7F7F7F7F-7....
+.|F--7||||||||FJ....
+.||.FJ||||||||L7....
+FJL7L7LJLJ||LJ.L-7..
+L--J.L7...LJS7F-7L7.
+....F-J..F7FJ|L7L7L7
+....L7.F7||L7|.L7L7|
+.....|FJLJ|FJ|F7|.LJ
+....FJL-7.||.||||...
+....L---J.LJ.LJLJ...
+The above sketch has many random bits of ground, some of which are in the loop (I) and some of which are outside it (O):
+
+OF----7F7F7F7F-7OOOO
+O|F--7||||||||FJOOOO
+O||OFJ||||||||L7OOOO
+FJL7L7LJLJ||LJIL-7OO
+L--JOL7IIILJS7F-7L7O
+OOOOF-JIIF7FJ|L7L7L7
+OOOOL7IF7||L7|IL7L7|
+OOOOO|FJLJ|FJ|F7|OLJ
+OOOOFJL-7O||O||||OOO
+OOOOL---JOLJOLJLJOOO
+In this larger example, 8 tiles are enclosed by the loop.
+
+Any tile that isn't part of the main loop can count as being enclosed by the loop. Here's another example with many bits of junk pipe lying around that aren't connected to the main loop at all:
+
+FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJ7F7FJ-
+L---JF-JLJ.||-FJLJJ7
+|F|F-JF---7F7-L7L|7|
+|FFJF7L7F-JF7|JL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L
+Here are just the tiles that are enclosed by the loop marked with I:
+
+FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJIF7FJ-
+L---JF-JLJIIIIFJLJJ7
+|F|F-JF---7IIIL7L|7|
+|FFJF7L7F-JF7IIL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L
+In this last example, 10 tiles are enclosed by the loop.
+
+Figure out whether you have time to search for the nest by calculating the area within the loop. How many tiles are enclosed by the loop?
 * */
